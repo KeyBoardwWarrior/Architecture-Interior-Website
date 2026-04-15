2f031cb8710320f3a488f2e8abc5913a1dc2622a
KeyBoardwWarrior <djassassin0510@gmail.com>
2026-04-15 11:54:38 +0530

    change

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+    <BrowserRouter basename="/Architecture-Interior-Website/">
       <App />
     </BrowserRouter>
 )
